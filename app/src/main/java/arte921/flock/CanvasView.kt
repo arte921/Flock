7f0938e9af7c7af730391b0371c0c883bc14462f
arte921 <ardjanve@gmail.com>
2020-02-15 20:33:38 +0100

    android studio commit

---
+++ app/src/main/java/arte921/flock/CanvasView.kt
@@ -0,0 +1,28 @@
+package arte921.flock
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.view.View
+import androidx.core.content.res.ResourcesCompat
+
+class CanvasView(context: Context): View(context) {
+    private lateinit var extraCanvas: Canvas
+    private lateinit var extraBitmap: Bitmap
+    private val skyColor = ResourcesCompat.getColor(resources,R.color.sky,null)
+    private val birdColor = ResourcesCompat.getColor(resources,R.color.bird,null)
+
+    override fun onSizeChanged(width:Int,height:Int,oldWidth:Int,oldHeight:Int){
+        super.onSizeChanged(width,height,oldWidth,oldHeight)
+        if(::extraBitmap.isInitialized) extraBitmap.recycle()
+        extraBitmap = Bitmap.createBitmap(width,height,Bitmap.Config.ARGB_8888)
+        extraCanvas = Canvas(extraBitmap)
+        extraCanvas.drawColor(skyColor)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawBitmap(extraBitmap, 0f, 0f, null)
+    }
+}
+
