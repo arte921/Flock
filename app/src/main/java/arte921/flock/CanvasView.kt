fa125b5b0f76f878f06de7e5b8af171145347ea0
arte921 <ardjanve@gmail.com>
2020-02-17 17:26:13 +0100

    android studio commit

--- app/src/main/java/arte921/flock/CanvasView.kt
+++ app/src/main/java/arte921/flock/CanvasView.kt
@@ -2,10 +2,13 @@ package arte921.flock
 
 import android.content.Context
 import android.graphics.*
+import android.util.Log
 import android.view.View
 import androidx.core.content.res.ResourcesCompat
+import java.lang.Math.PI
 import java.lang.Math.random
 import java.util.*
+import kotlin.math.cos
 
 
 class CanvasView(context: Context): View(context) {
@@ -14,11 +17,12 @@ class CanvasView(context: Context): View(context) {
     private val skyColor = ResourcesCompat.getColor(resources,R.color.sky,null)
     private val birdColor = ResourcesCompat.getColor(resources,R.color.bird,null)
     private val sw = 12f
-    private lateinit var frame: Rect
     private var go: Long = 0
     private var deltaT: Long = 0
     private var boids = MutableList(20) { boid() }
-
+    private lateinit var currentBoid: boid
+    private var nearbyBoids = mutableListOf<boid>()
+    private var adjustedAngle: Double = 0.0
 
     private val paint = Paint().apply {
         color = birdColor
@@ -45,21 +49,52 @@ class CanvasView(context: Context): View(context) {
         canvas.drawBitmap(extraBitmap, 0f, 0f, null)
 
         boids.forEach {
-
+            currentBoid = it
+            nearbyBoids.clear()
 
             boids.forEach {
-
-
-
+                if(currentBoid.getDistance(it.x,it.y) < currentBoid.viewRadius && it !== currentBoid){
+                    nearbyBoids.add(it)
+                }
             }
 
+            if(nearbyBoids.size > 0){
+                nearbyBoids.forEach {
+                    currentBoid.dspeed += it.speed
+                    adjustedAngle = it.angle - currentBoid.angle
+                    if(adjustedAngle<0) adjustedAngle = 2 * PI - adjustedAngle
+
+                    if(it.angle > PI){
+                        currentBoid.dangle += it.angle - 2 * PI
+                    }else{
+                        currentBoid.dangle += it.angle
+                    }
+                }
+
+                currentBoid.speed = currentBoid.dspeed / nearbyBoids.size
+                if(currentBoid.dangle < 0){
+                    currentBoid.angle = 2 * PI - currentBoid.dangle
+                }else{
+                    currentBoid.angle = currentBoid.dangle
+                }
+            }
+
+            currentBoid.x = currentBoid.x + deltaT * currentBoid * cos()
+
+
+
+            currentBoid.reset()
+
+            currentBoid.log()
+
+            canvas.drawPoint(currentBoid.x.toFloat(),currentBoid.y.toFloat(),paint)
 
         }
 
 
+
         deltaT = Calendar.getInstance().timeInMillis - go
         go = Calendar.getInstance().timeInMillis
-        invalidate()
+        //invalidate()
     }
 }
-
--- app/src/main/java/arte921/flock/boid.kt
+++ app/src/main/java/arte921/flock/boid.kt
@@ -1,31 +1,42 @@
 package arte921.flock
 
+import android.util.Log
+import java.lang.Math.random
 import kotlin.math.PI
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 class boid {
-    var x: Float
-    var y: Float
-    var angle: Float
-    var speed: Float
-    var acceleration: Float
-    var viewRadius: Float
-
-    var dspeed: Float
-    var dangle: Float
-
+    var x: Double
+    var y: Double
+    var angle: Double
+    var speed: Double
+    var acceleration: Double
+    var viewRadius: Double
+    var dspeed: Double
+    var dangle: Double
 
     init {
-        this.x = 300.0F
-        this.y = 300.0F
-        this.angle = PI.toFloat()
-        this.speed = 10.0F
-        this.acceleration = 0.0F
-        this.viewRadius = 10.0F
-        this.dspeed = this.speed
+        this.x = random() * 300.0
+        this.y = random() * 300.0
+        this.angle = random() * 2 * PI
+        this.speed = random() * 10
+        this.acceleration = 0.0
+        this.viewRadius = 10.0
+        this.dspeed = 0.0
         this.dangle = this.angle
     }
 
-    fun getDistance(x,y){
-        return
+    fun getDistance(x: Double,y: Double): Double {
+        return sqrt((x-this.x).pow(2) + (y-this.y).pow(2))
+    }
+
+    fun log(){
+        Log.i("boid.log",this.x.toString() + "," +  this.x.toString() + ", Angle: " +  this.angle.toString() + ", Speed: " +  this.speed.toString())
+    }
+
+    fun reset(){
+        this.dspeed = 0.0
+        this.dangle = 0.0
     }
 }
